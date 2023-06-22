938a7aa9957e2164719c418268d58394f239f983
RalphBrynard <ralph@thebrynards.com>
2023-06-22 11:37:36 -0500

    removed override.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "resource_group" {
   source          = "github.com/D3V0PSPL38/terraform-azurerm-resource-group.git"
   count           = var.create_resource_group ? 1 : 0
   enabled         = module.this.enabled
-  name            = var.name
+  name            = join("-", [var.resource_group_label.name, var.name])
   namespace       = var.namespace
   attributes      = var.attributes
   label_order     = var.label_order
